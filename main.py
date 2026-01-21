74b956cfe2054cd43d2440a73b7c4e38e8ec1c02
Kaitlynn Yang <yang.kaitlynn@northeastern.edu>
2026-01-21 18:32:54 -0500

    commit 2

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+main.py
+"""
+
+from colors import C
+from queue_system import QueueSystem
+
+sequence = [C.g, C.r, C.p, C.b, C.w]
+qs = QueueSystem(sequence)
+
+while True:
+    i = input(f"{qs} : ")
+    if i == "q":
+        qs.dump()
+    elif i == "w":
+        qs.w()
+    else:
+        break
---
+++ queue_system.py
@@ -0,0 +1,35 @@
+"""
+Module for class QueueSystem (system for representing the queue)
+"""
+
+class QueueSystem:
+    """
+    Class QueueSystem
+    """
+    def __init__(self, seq : list[str]) -> None:
+        self.seq = seq
+        return
+
+    def __str__(self) -> str:
+        return " ".join(self.seq)
+
+    def w(self) -> "QueueSystem":
+        """w"""
+        if len(self.seq) >= 2:
+            self.seq[0], self.seq[1] = self.seq[1], self.seq[0]
+        return self
+
+    def dump(self) -> "QueueSystem":
+        """dump"""
+        if self.seq:
+            self.seq.append(self.seq.pop(0))
+        self.w()
+        return self
+
+if __name__ == "__main__":
+    obj : QueueSystem = QueueSystem(["1","2","3","4","5"])
+    print(obj)
+    obj.w()
+    print(obj)
+    obj.dump()
+    print(obj)
